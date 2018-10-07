d4acb8d56bc0b67d2f7771802e6fa94c7d6906bc
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-07 20:27:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,6 +9,33 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 puts "Welcome to Tic Tac Toe!"
 display_board(board)
 puts "Where would X like to go? "
-decision = gets.chomp
+decision1 = gets.chomp
+if decision1 = 1
+  board[0] = "X"
+end
+if decision1 = 2
+  board[1] = "X"
+end
+if decision1 = 3
+  board[2] = "X"
+end
+if decision1 = 4
+  board[3] = "X"
+end
+if decision1 = 5
+  board[4] = "X"
+end
+if decision1 = 6
+  board[5] = "X"
+end
+if decision1 = 7
+  board[6] = "X"
+end
+if decision1 = 8
+  board[7] = "X"
+end
+if decision1 = 9
+  board[8] = "X"
+end
 system("clear")
-
+display_board(board)
